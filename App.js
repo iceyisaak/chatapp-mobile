cb248924571b6d7edde2f5adadadf2f2ee675af7
iceyisaak <me@iceyisaak.com>
2021-10-13 14:19:55 +0200

    Created a new Expo app

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import AuthNavigator from './src/navigation/AuthNavigator';
+import { StyleSheet, Text, View } from 'react-native';
 
-
-import LoginScreen from './src/components/Login';
-import globalStyle from './src/styles/GlobalStyle';
-
-
-const App = () => {
+export default function App() {
   return (
-    <>
-      {/* <LoginScreen style={globalStyle} /> */}
-      <AuthNavigator />
-    </>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
 
-export default App;
-
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
